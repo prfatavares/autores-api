b34168192bcba5c63178e5c8643aa4d6fdec3e79
Alexandre Tavares <alexandre@atavares.com.br>
2025-11-07 00:50:09 -0300

    Atualizado Spring Boot e incluídas configurações de usuários em memória.

--- src/main/java/br/com/atavares/autoresapi/config/SecurityConfiguration.java
+++ src/main/java/br/com/atavares/autoresapi/config/SecurityConfiguration.java
@@ -2,10 +2,18 @@ package br.com.atavares.autoresapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,15 +21,40 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(Customizer.withDefaults())
+                //.formLogin(Customizer.withDefaults()) // Alternativa: spring-boot-starter-thymeleaf
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(authorizeRequests -> {
+                    //authorizeRequests.requestMatchers("/autores/**").hasAnyRole("USER", "ADMIN");
+                    authorizeRequests.requestMatchers(HttpMethod.POST, "/autores/**").hasRole("ADMIN");
+                    //authorizeRequests.requestMatchers(HttpMethod.GET, "/autores/**").hasAuthority("PESQUISAR_AUTOR");
                     authorizeRequests.anyRequest().authenticated();
+                    //authorizeRequests.anyRequest().permitAll();
+                    //authorizeRequests.anyRequest().denyAll();
+
                 })
                 .build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user1 = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("123456"))
+                .roles("ADMIN")
+                .build();
+        UserDetails user2 = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("123456"))
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
 }
